2281426ae4ee01f0a2c6b93a23d02a9983777692
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2024-08-20 18:03:20 +0300

    eh eh

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -12,32 +12,13 @@
 //   }
 // }; старая
 
-// import createHttpError from 'http-errors';
-
-// export const validateBody = (schema) => async (req, res, next) => {
-//   try {
-//     await schema.validateAsync(req.body, {
-//       abortEarly: false, 
-//       convert: false,
-//     });
-//     next();
-//   } catch (err) {
-//     const errorMessages = err.details.map(detail => detail.message);
-//     const error = createHttpError(400, 'Validation error', {
-//       errors: errorMessages,
-//     });
-//     next(error);
-//   }
-// };
-// вроде тут тоже гуд
-
 import createHttpError from 'http-errors';
 
 export const validateBody = (schema) => async (req, res, next) => {
   try {
-    await schema.validateAsync(req.body, { 
+    await schema.validateAsync(req.body, {
       abortEarly: false, 
-      convert: false 
+      convert: false,
     });
     next();
   } catch (err) {
@@ -45,13 +26,7 @@ export const validateBody = (schema) => async (req, res, next) => {
     const error = createHttpError(400, 'Validation error', {
       errors: errorMessages,
     });
-    if (req.accepts('json')) {
-      res.status(400).json({ 
-        message: 'Validation error',
-        errors: errorMessages 
-      });
-    } else {
-      next(error);
-    }
+    next(error);
   }
 };
+// вроде тут тоже гуд
